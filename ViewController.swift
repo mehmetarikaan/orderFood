2c0361e6b1f79ab4b489a8011ea54d63b7131341
Mehmet Arikan <mehmetarikaan@outlook.com>
2022-07-18 14:13:20 +0300

    bux fix at ViewController

--- ViewController.swift
+++ ViewController.swift
@@ -20,11 +20,11 @@ class ViewController: UIViewController {
         foodsTableView.delegate = self
         
         // nesneler oluşturuldu
-        let y1 = Foods(foodId: 1, foodName: "Ayran", foodPhotoName: "ayran", foodPrice: 2.15)
-        let y2 = Foods(foodId: 2, foodName: "Baklava", foodPhotoName: "baklava", foodPrice: 2.15)
-        let y3 = Foods(foodId: 3, foodName: "Fanta", foodPhotoName: "fanta", foodPrice: 2.15)
-        let y4 = Foods(foodId: 4, foodName: "Izgara Somon", foodPhotoName: "izgarasomon", foodPrice: 2.15)
-        let y5 = Foods(foodId: 5, foodName: "Izgara Tavuk", foodPhotoName: "izgaratavuk", foodPrice: 2.15)
+        let y1 = Foods(foodId: 1, foodName: "Ayran", foodPhotoName: "ayran", foodPrice: 2.39)
+        let y2 = Foods(foodId: 2, foodName: "Baklava", foodPhotoName: "baklava", foodPrice: 15.0)
+        let y3 = Foods(foodId: 3, foodName: "Fanta", foodPhotoName: "fanta", foodPrice: 245)
+        let y4 = Foods(foodId: 4, foodName: "Izgara Somon", foodPhotoName: "izgarasomon", foodPrice: 15)
+        let y5 = Foods(foodId: 5, foodName: "Izgara Tavuk", foodPhotoName: "izgaratavuk", foodPrice: 54.4)
         let y6 = Foods(foodId: 6, foodName: "Kadayıf", foodPhotoName: "kadayif", foodPrice: 2.15)
         let y7 = Foods(foodId: 7, foodName: "Kahve", foodPhotoName: "kahve", foodPrice: 2.15)
         let y8 = Foods(foodId: 8, foodName: "Köfte", foodPhotoName: "kofte", foodPrice: 2.15)
@@ -44,42 +44,48 @@ class ViewController: UIViewController {
         foodList.append(y9)
         foodList.append(y10)
         
+        for food in foodList{
+            print(food.foodName!)
+        }
+
         
+        }
+    
+        }
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource,FoodTableViewCellProtocol {
+    
+    func toOrder(indexPath: IndexPath) {
+        let gelenYemek = foodList[indexPath.row]
+        print("\(gelenYemek.foodName!) sipariş verildi.Kazanç \(gelenYemek.foodPrice!)")
     }
-
-
-}
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource, FoodTableViewCellProtocol {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return foodList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let getFoods = foodList[indexPath.row]
+        let gelenYemek = foodList[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "yemekHucre", for: indexPath) as! FoodTableViewCell
         
-        cell.foodNameLabel.text = getFoods.foodName
-        cell.foodPriceLabel.text = "\(getFoods.foodPrice!) TL"
-        cell.foodPhotoImageView.image = UIImage(named: getFoods.foodPhotoName!)
-        
+        cell.foodNameLabel.text = gelenYemek.foodName
+        cell.foodPriceLabel.text = "\(gelenYemek.foodPrice!) TL"
+        cell.foodPhotoImageView.image = UIImage(named: gelenYemek.foodPhotoName!)
         
         cell.hucreProtocol = self
         cell.indexPath = indexPath
         
         return cell
+    
     }
     
-    func toOrder(indexPath: IndexPath) {
-        let getFoods = foodList[indexPath.row]
-        print("\(getFoods.foodName!) sipariş verildi.")
-    }
     
     
 }
